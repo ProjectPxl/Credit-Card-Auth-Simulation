33cd9574b8d094df435d50e12d3c4e09fbf15548
Paul Maltsev <paulmaltsev@Pauls-MacBook-Air.local>
2014-09-17 17:16:56 -0700

    merging when cases into one

--- lib/init.rb
+++ lib/init.rb
@@ -17,11 +17,8 @@ File.readlines(ARGV[0]).each do |line|
     when "Add"
       add(action[1], action[2], action[3][1..-1].to_i, instance)
 
-    when "Charge"
-      charge_or_credit(action[1], action[2][1..-1].to_i, "charge", instance)
-
-    when "Credit"
-      charge_or_credit(action[1], action[2][1..-1].to_i, "credit", instance)
+    when "Charge", "Credit"
+      charge_or_credit(action[1], action[2][1..-1].to_i, action[0].downcase, instance)
 
     else
       puts "Unknown action specified."
